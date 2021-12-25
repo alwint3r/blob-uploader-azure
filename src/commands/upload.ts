cab5cad20cc752a94c4874c4e2b4aef4606abb95
Alwin Arrasyid <alwin.ridd@gmail.com>
2021-12-25 18:26:17 +0700

    feat: read token from a file if not provided as CLI args

--- src/commands/upload.ts
+++ src/commands/upload.ts
@@ -1,5 +1,8 @@
 import { BlobServiceClient } from "@azure/storage-blob";
-import { basename } from "path";
+import { parse } from "dotenv";
+import { readFile, stat } from "fs/promises";
+import { homedir } from "os";
+import { basename, join } from "path";
 import { CommandBuilder } from "yargs";
 
 export const command = "upload";
@@ -10,7 +13,7 @@ type Options = {
   file: string;
   container: string;
   account: string;
-  token: string;
+  token?: string;
 };
 
 export const builder: CommandBuilder<Options, Options> = (yargs) =>
@@ -37,12 +40,15 @@ export const builder: CommandBuilder<Options, Options> = (yargs) =>
       alias: "t",
       describe: "The token to use for authentication",
       type: "string",
-      demandOption: true,
+      demandOption: false,
     });
 
 export const handler = async (argv: Options) => {
   const { file, container, account, token } = argv;
-  const blobService = new BlobServiceClient(getBlobServiceUrl(account, token));
+  const usedToken = token || (await resolveToken());
+  const blobService = new BlobServiceClient(
+    getBlobServiceUrl(account, usedToken)
+  );
   const containerClient = blobService.getContainerClient(container);
 
   const filename = basename(file);
@@ -53,4 +59,14 @@ export const handler = async (argv: Options) => {
 
 function getBlobServiceUrl(accountName: string, token: string): string {
   return `https://${accountName}.blob.core.windows.net/?${token}`;
+}
+
+async function resolveToken(): Promise<string> {
+  const defaultPath = join(homedir(), ".blob-uploader-azure.env");
+  if (await stat(defaultPath)) {
+    const { TOKEN: token } = parse(await readFile(defaultPath));
+    return token;
+  }
+
+  throw new Error("No token provided");
 }
